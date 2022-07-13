404295a8a80b0d24a898bc940263423bca9eaba8
therealbryanho <therealbryanho@gmail.com>
2022-07-13 15:35:36 +0800

    first commit

--- index.js
+++ index.js
@@ -31,33 +31,20 @@ app.use(async (req, res, next) => {
 
 app.get('/', (req, res) => {
   const name = req.cookies.jwtSet ? 'user' : 'guest';
-
+  /*
+  Humanode NFT Minter
+   - If authentication was successful, it would have passed the NFT smart contract control mechanism
+   - We will still run a test against the cookie to ensure again
+   - showmintbtn true will then show the MINT button on frontend 
+  */ 
+  let showmintbtn = 'false';
+  if(name == 'user'){
+    const jwtSet = req.cookies.jwtSet ? new openid.TokenSet(req.cookies.jwtSet) : undefined;
+    showmintbtn = (req.cookies.jwtSet && jwtSet.claims().aud.includes('hackathon-participant') && jwtSet.claims().iss === 'https://auth.staging.oauth2.humanode.io/') ? 'true' : 'false';
+  }
   res.render('index', {
-    name
+    name, showmintbtn
   });
-});
-
-app.get('/secret', (req, res) => {
-  // Get JWT.
-  const jwtSet = req.cookies.jwtSet
-    ? new openid.TokenSet(req.cookies.jwtSet) : undefined;
-
-  // Make a little inspection on JWT claims
-  // and render secret page if claims are correct,
-  // redirect back on root otherwise.
-  if (
-    // Check if JWT is present.
-    jwtSet
-      // Check if JWT claims the correct client-id.
-      && jwtSet.claims().aud.includes('hackathon-participant')
-      // Check if JWT claims the correct issuer.
-      && jwtSet.claims().iss === 'https://auth.staging.oauth2.humanode.io/'
-  ) {
-    res.render('secret');
-  }
-  else {
-    res.redirect('/');
-  }
 });
 
 app.get('/login', async (req, res) => {
@@ -103,6 +90,15 @@ app.get('/callback', async (req, res) => {
   const tokenSet = await client.callback('http://localhost:3000/callback', params, { state: 'some-state', code_verifier: codeVerifier });
   // Save JWT.
   res.cookie('jwtSet', tokenSet, { maxAge: 360000 });
+
+  /*
+  Humanode NFT Minter
+   - Run a call to NFT smart contract and check if current user's wallet is already holding max amount of NFT (if this is a desired control) 
+   - If pass the smart contract control checks, render the MINT button on main page 
+  */ 
+  // Save showMintButton (for this demo. with assumption that smart contract control checks are passed.) 
+  res.cookie('showMintButton', tokenSet, { maxAge: 360000 });
+
   // Redirect end-user to root route.
   res.redirect('/');
 });
